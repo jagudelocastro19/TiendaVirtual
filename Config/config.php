7032ed3a045bcaadf0f3c950767498c0be547a25
Sebastian Agudelo <j.agudelocastro@gmail.com>
2022-08-13 01:24:09 -0500

    cambios en el config y load

---
+++ Config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+    const BASE_URL = "http://localhost/repo/TiendaVirtual/";
+    const LIBS = "Libraries/";
+    const VIEWS = "Views/";
+?>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-    $url = !empty($GET['url']) ? $_GET['url'] : 'home/home';
+    require_once("Config/config.php");
+    $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home'; 
+    
+
     $arrUrl = explode("/", $url);
     $controller = $arrUrl[0];
     $method = $arrUrl[0];
@@ -26,11 +29,35 @@
         }
     }
 
-    echo "<br>";
-    echo "controlador: ".$controller;
-    echo "<br>";
-    echo "método:".$method;
-    echo "<br>";
-    echo "parametros:".$params;
+    spl_autoload_register(function($class){
+        //echo LIBS.'Core/'.$class.".php";
+        if(file_exists(LIBS.'Core/'.$class.".php")){
+            require_once(LIBS.'Core/'.$class.".php");
+        }
+    });
+
+    //LOAD
+    $controllerFile = "Controllers/".$controller.".php";
+    if(file_exists($controllerFile))
+    {
+        require_once($controllerFile);
+        $controller = new $controller();
+        if(method_exists($controller, $method))
+        {
+            $controller->{$method}($params);
+        }else{
+            echo "No exite el método";
+        }
+
+    }else{
+        echo "No existe el controlador";
+    }
+
+    // echo "<br>";
+    // echo "controlador: ".$controller;
+    // echo "<br>";
+    // echo "método:".$method;
+    // echo "<br>"; 
+    // echo "parametros:".$params;
 
 ?>
